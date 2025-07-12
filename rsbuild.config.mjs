c98c1cdede4fd58dc3685564eacfd74955b68e02
Will Heslam <willheslam@gmail.com>
2025-07-12 12:04:26 +0100

    Configure rsbuild

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core"
+import { pluginReact } from "@rsbuild/plugin-react"
 
 export default defineConfig({
   plugins: [pluginReact()],
-});
+  output: {
+    distPath: {
+      root: "docs",
+    },
+    assetPrefix: "/tech-test-4410/",
+  },
+})
